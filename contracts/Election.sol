2ccff8c5c11dac96e6c28166e8cc5a31419c965b
Rogith-Ranger <rogithy@gmail.com>
2019-07-26 11:30:39 +0530

    fix 1

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -13,9 +13,14 @@ contract Election {
 
     uint public candidatesCount;
 
+    event votedEvent (
+    	uint indexed _candidateId
+    	);
+
     constructor () public {
    		addCandidate("Candidate 1");
-   		addCandidate("RoTam");
+   		addCandidate("Candidate 2");
+   		addCandidate("Candidate 3");
     }
 
     function addCandidate (string memory _name) private {
@@ -28,5 +33,6 @@ contract Election {
     	require(_candidateId >0 && _candidateId <= candidatesCount);
     	voters[msg.sender] = true;
     	candidates[_candidateId].voteCount++;
+    	emit votedEvent(_candidateId);
     }
 }
